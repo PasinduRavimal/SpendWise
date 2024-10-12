a1bfdf060b6ba75eac5d31a79b4b4bd5492636b3
Pasindu Ravimal <34201061+PasinduRavimal@users.noreply.github.com>
2024-10-12 23:32:29 +0530

    Initial Setup

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
-public class App {
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.scene.text.*;
+import javafx.scene.layout.*;
+
+public class App extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Text text = new Text(69, 89, "Welcome to JavaFX UI Programming!");
+        text.setFont(new Font(16));
+
+        Pane pane = new Pane(text);
+        pane.setPrefSize(400, 200);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(new Scene(pane));
+        primaryStage.show();
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        launch(args);
     }
 }
