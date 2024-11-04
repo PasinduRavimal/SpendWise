ebf266abbceb0c6650c69da5ef62216246c73778
HashiniSewmini <hashinisewmini01@gmail.com>
2024-11-04 13:01:30 +0530

    Added some methods to UserAccount.java

--- src/com/spendWise/models/UserAccount.java
+++ src/com/spendWise/models/UserAccount.java
@@ -24,4 +24,46 @@ public interface UserAccount {
             DatabaseConnection.getStatement().close();
         }
     }
+
+    public static boolean createUserAccount(String userName, String displayName, String password) throws SQLException{
+        try{
+            DatabaseConnection.getConnection();
+            String query = "INSERT INTO Users (username, displayName, password) VALUES ('"+userName+"', '"+displayName+"', '"+password+"')";
+            DatabaseConnection.getStatement().executeUpdate(query);
+            return true;
+
+        } catch(Exception e){
+            return false;
+        } finally{
+            DatabaseConnection.getStatement().close();
+        }
+    }
+
+    public static boolean updateUserAccount(String username, String newDisplayName) throws SQLException{
+        try{
+            DatabaseConnection.getConnection();
+            String query = "UPDATE Users SET displayName = '"+newDisplayName+"' WHERE username = '"+username+"'";
+            DatabaseConnection.getStatement().executeUpdate(query);
+            return true;
+
+        } catch(Exception e){
+            return false;
+        } finally{
+            DatabaseConnection.getStatement().close();
+        }
+    }
+
+    public static boolean deleteUserAccount(String username) throws SQLException{
+        try{
+            DatabaseConnection.getConnection();
+            String query = "DELETE FROM Users WHERE username = '"+username+"'";
+            DatabaseConnection.getStatement().executeUpdate(query);
+            return true;
+            
+        } catch(Exception e){
+            return false;
+        } finally {
+            DatabaseConnection.getStatement().close();
+        }
+    }
 }
