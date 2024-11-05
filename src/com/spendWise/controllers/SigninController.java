def2d6df307bc0e40d9bef733439dd7491ce4abe
Ayeshini <kulathungaayeshini@gmail.com>
2024-11-05 14:59:35 +0530

    added new lines to signin.fxml and SigninController.java

--- src/com/spendWise/controllers/SigninController.java
+++ src/com/spendWise/controllers/SigninController.java
@@ -4,8 +4,18 @@ import java.net.*;
 import java.util.*;
 
 import javafx.fxml.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class SigninController implements Initializable {
+
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Button signupButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
