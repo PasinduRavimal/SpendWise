cd46dfc4cb5b80c6bd1f558719fb6020bd8f4a94
Pasindu Ravimal <34201061+PasinduRavimal@users.noreply.github.com>
2024-11-06 22:33:42 +0530

    Home navigation set

--- src/com/spendWise/controllers/HomeController.java
+++ src/com/spendWise/controllers/HomeController.java
@@ -1,11 +1,15 @@
 package com.spendWise.controllers;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TitledPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 
 public class HomeController implements Initializable {
 
@@ -15,13 +19,44 @@ public class HomeController implements Initializable {
     private TitledPane accountsPane;
     @FXML private TitledPane settingsPane;
     @FXML private TitledPane helpPane;
+    @FXML private Pane contentPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        ContentController contentController = new ContentController();
+
         dashboardPane.setOnMouseClicked(event -> {
-            System.out.println("Dashboard clicked");
+            contentController.setContent("dashboard");
+        });
+        settingsPane.setOnMouseClicked(event -> {
+            contentController.setContent("settings");
+        });
+        helpPane.setOnMouseClicked(event -> {
+            contentController.setContent("help");
         });
 
+    }
+
+    class ContentController {
+        private HashMap<String, Pane> contentMap = new HashMap<>();
+        
+        public ContentController() {
+            try{
+            contentMap.put("dashboard", FXMLLoader.load(getClass().getResource("../views/DashboardContent.fxml")));
+            contentMap.put("accounts", new Pane());
+            contentMap.put("settings", FXMLLoader.load(getClass().getResource("../views/SettingsContent.fxml")));
+            contentMap.put("help", new Pane());
+            } catch (IOException e){
+                e.printStackTrace();
+                Alert alert = new Alert(Alert.AlertType.ERROR, "An error occurred while loading the content");
+                alert.showAndWait();
+            }
+        }
+
+        public void setContent(String key) {
+            contentPane.getChildren().clear();
+            contentPane.getChildren().add(contentMap.get(key));
+        }
     }
     
 }
