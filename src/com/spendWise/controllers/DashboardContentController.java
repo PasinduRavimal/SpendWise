a952a7998a87fc93584281b6a6bfd96bfed019c5
HashiniSewmini <hashinisewmini01@gmail.com>
2024-11-09 10:58:35 +0530

    Modified AddTransaction.fxml, CorrectTransactionError.fxml, DashbordContent.fxml DashboardContentController.java

--- src/com/spendWise/controllers/DashboardContentController.java
+++ src/com/spendWise/controllers/DashboardContentController.java
@@ -1,39 +1,54 @@
 package com.spendWise.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.IOException;
-import java.net.*;
-import java.sql.SQLException;
-import java.util.*;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-import com.spendWise.models.UserAccount;
-
-import javafx.fxml.*;
-import javafx.scene.control.*;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.shape.Rectangle;
-
-
-public class DashboardContentController implements Initializable {
-
-    @FXML 
-    private Label LabelCashBalance, LabelBankBalance, LabelTotalBalance;
+public class DashboardContentController  implements Initializable {
 
     @FXML
-    private Button ButtonAddTransaction, ButtonCorrectError, ButtonUndo;
-
-    @FXML
-    private Rectangle RectangleCashBalance, RectangleBankBalance, RectangleTotalBalance;
-
-    @FXML
-    private TabPane TabPaneAccountDetails, TabPaneTransactionHistory;
-
-    @FXML
-    private TextField TextDate;
+    private Button ButtonAddTransaction, ButtonCorrectError;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         
+        ButtonAddTransaction.setOnAction(event -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/addTransaction.fxml"));
+                Parent addTransactionRoot = loader.load();
+                Scene addTransactionScene = new Scene(addTransactionRoot);
+                Stage currentStage = (Stage) ButtonAddTransaction.getScene().getWindow();
+                currentStage.setScene(addTransactionScene);
+                currentStage.setTitle("Add Transaction");
+                currentStage.centerOnScreen(); 
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        ButtonCorrectError.setOnAction(event -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/correctTransactionError.fxml"));
+                Parent correctErrorRoot = loader.load();
+                Scene correctErrorScene = new Scene(correctErrorRoot);
+                Stage currentStage = (Stage) ButtonCorrectError.getScene().getWindow();
+                currentStage.setScene(correctErrorScene);
+                currentStage.setTitle("Correct Transaction Error");
+                currentStage.centerOnScreen(); 
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
+
     
 }
  
